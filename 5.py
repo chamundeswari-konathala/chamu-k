8aad329db4b2636055bfff9d0f4c6222ef3aeaa1
chamundeswari-konathala <60577044+chamundeswari-konathala@users.noreply.github.com>
2020-02-08 10:03:44 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,31 @@
+print("1@#$%(())%$$@$#")
+#id comparision:
+
+ld=[[1,10,'cs'],[2,20,'sc'],[3,30,'pk']]
+print(ld[0])
+print(ld[1])
+print(ld[2])
+
+print(ld[0][0])
+print(ld[1][1])
+print(ld[2][2])
+print(ld[0][1])
+
+
+
+for x,y,z in ld:
+    print(x)
+    print(y)
+
+
+#data checking:
+data=[1,[[10,'sc',10],3],"apple"]
+print(data[1])
+print(data[1][1])
+#id checking
+print("1@#$%(())%$$@$#")
+ld1=[10,20,[1,['cs','sc','man']]]
+print(ld1[2][1])
+print(ld1[2][1][1])
+print(ld1[2][1][1][-1])
+
